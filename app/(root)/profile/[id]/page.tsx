f010879323dc0c33b012743fc6d7e00951482b0f
Karan Panchal <karanhpanchal09@gmail.com>
2025-05-25 23:05:19 +0530

    Home page ui done with dummy video card data

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,4 +1,6 @@
 import Header from "@/components/Header";
+import VideoCard from "@/components/VideoCard";
+import { dummyCards } from "@/constants";
 import React from "react";
 
 const page = async ({ params }: ParamsWithSearch) => {
@@ -7,9 +9,11 @@ const page = async ({ params }: ParamsWithSearch) => {
     <div className="wrapper page">
         <Header subHeader="karanhpanchal09@gmail.com" title="Karan Panchal"userImg='/assets/images/dummy.jpg'/>
         
-        <h1 className="text-2xl font-karla">
-            user id: {id}
-        </h1>
+        <section className="video-grid">
+         {dummyCards.map((card) => (
+        <VideoCard key={card.id} {...card} />
+      ))}
+      </section>
         </div>
   )
 };
