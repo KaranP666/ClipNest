aa8df8f21a8901dea79f06fcfb064da6b510df60
Karan Panchal <karanhpanchal09@gmail.com>
2025-06-04 00:31:23 +0530

    basic cdn setup on bunny is done

--- constants/index.ts
+++ constants/index.ts
@@ -3,10 +3,10 @@ export const MAX_THUMBNAIL_SIZE = 10 * 1024 * 1024;
 
 export const BUNNY = {
   STREAM_BASE_URL: "https://video.bunnycdn.com/library",
-  STORAGE_BASE_URL: "https://sg.storage.bunnycdn.com/snapcast",
-  CDN_URL: "https://snapcast.b-cdn.net",
   EMBED_URL: "https://iframe.mediadelivery.net/embed",
-  TRANSCRIPT_URL: "https://vz-47a08e64-84d.b-cdn.net",
+  STORAGE_BASE_URL: "https://storage.bunnycdn.com/clipnest",
+  CDN_URL: "https://clipnest.b-cdn.net",
+  TRANSCRIPT_URL: "https://vz-2f9b7694-3df.b-cdn.net",
 };
 
 export const emojis = ["😂", "😍", "👍"];
